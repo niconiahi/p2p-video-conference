e953944f6b459f7da84e7ce27456d3ed50999147
Nicolas Accetta <nicolas.accetta@gmail.com>
2025-11-12 11:30:13 -0300

    chore: remove logs + supress node deprecation warning

--- workers/app.ts
+++ workers/app.ts
@@ -20,7 +20,6 @@ export { Broadcaster };
 export default {
   async fetch(request, env, ctx) {
     const upgrade_header = request.headers.get("Upgrade");
-    console.log("upgrade_header", upgrade_header);
     if (upgrade_header === "websocket") {
       const url = new URL(request.url);
       const host = url.searchParams.get("host");
@@ -28,9 +27,7 @@ export default {
         throw new Error('"host" search param is required');
       }
       const id = env.BROADCASTER.idFromName(host);
-      console.log("id", id);
       const stub = env.BROADCASTER.get(id);
-      console.log("stub", stub);
       return stub.fetch(request);
     }
     return requestHandler(request, {
